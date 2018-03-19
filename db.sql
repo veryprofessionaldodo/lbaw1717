17f75f4366788785bbb32a291a32672b7fa34fb7
MeBeeDaisy <up201505505@gcloud.fe.up.pt>
2018-03-19 22:30:07 +0000

    database updated

--- db.sql
+++ db.sql
@@ -4,35 +4,33 @@ CREATE TABLE User (
 	username text NOT NULL,
 	email text NOT NULL,
 	image text,
-	password text NOT NULL,
-	CONSTRAINT usernameCheck CHECK ((username != 'admin'))
+	password text NOT NULL
 );
 
 CREATE TABLE Project (
 	id SERIAL NOT NULL,
 	name text NOT NULL,
 	description text NOT NULL,
-	access text NOT NULL,
-	CONSTRAINT access CHECK ((access = ANY(ARRAY['Private'::text, 'Public'::text])))
+	isPublic boolean NOT NULL
 );
 
 CREATE TABLE Sprint (
 	id SERIAL NOT NULL,
 	name text NOT NULL,
 	deadline TIMESTAMP WITH TIME zone NOT NULL,
-	effort text NOT NULL,
+	effort INTEGER NOT NULL,
 	project_id INTEGER NOT NULL,
 	user_creator_id INTEGER NOT NULL,
-	CONSTRAINT deadline CHECK (deadline > now()),
-	CONSTRAINT effort CHECK ((effort = ANY(ARRAY['Low'::text, 'Medium'::text, 'High'::text])))
+	CONSTRAINT deadline CHECK (deadline > now())
 );
 
 CREATE TABLE Task (
 	id SERIAL NOT NULL,
 	name text NOT NULL,
 	description text,
-	effort text NOT NULL,
-	CONSTRAINT effort CHECK ((effort = ANY(ARRAY['Low'::text, 'Medium'::text, 'High'::text])))
+	effort INTEGER NOT NULL,
+	project_id INTEGER NOT NULL,
+	sprint_id INTEGER
 );
 
 CREATE TABLE Thread (
@@ -48,7 +46,10 @@ CREATE TABLE Comment (
 	id SERIAL NOT NULL,
 	content text NOT NULL,
 	"date" TIMESTAMP WITH TIME zone DEFAULT now() NOT NULL,
-	user_id INTEGER NOT NULL
+	user_id INTEGER NOT NULL,
+	task_id INTEGER,
+	thread_id INTEGER,
+	CONSTRAINT belongs CHECK ((task_id != NULL AND thread_id = NULL) OR (task_id = NULL AND thread_id != NULL))
 );
 
 CREATE TABLE Category (
@@ -59,9 +60,8 @@ CREATE TABLE Category (
 CREATE TABLE Project_members (
 	user_id INTEGER NOT NULL,
 	project_id INTEGER NOT NULL,
-	name text NOT NULL,
-	"date" TIMESTAMP WITH TIME zone DEFAULT now() NOT NULL,
-	CONSTRAINT name CHECK ((name = ANY(ARRAY['Team Member'::text, 'Coordinator'::text])))
+	isCoordinator boolean NOT NULL,
+	"date" TIMESTAMP WITH TIME zone DEFAULT now() NOT NULL
 );
 
 CREATE TABLE Administrator (
@@ -70,17 +70,17 @@ CREATE TABLE Administrator (
 	password text NOT NULL
 );
 
-CREATE TABLE Image(
-	id SERIAL NOT NULL,
-	url text NOT NULL
-);
-
 CREATE TABLE Report (
 	id SERIAL NOT NULL,
 	"date" TIMESTAMP WITH TIME zone DEFAULT now() NOT NULL,
+	type text NOT NULL,
 	summary text NOT NULL,
-	content text NOT NULL,
-	user_id INTEGER NOT NULL
+	user_id INTEGER NOT NULL,
+	comment_reported_id INTEGER,
+	user_reported_id INTEGER,
+	CONSTRAINT reportType CHECK ((type = ANY(ARRAY['CommentReported'::text, 'UserReported'::text]))),
+	CONSTRAINT typeConstraint CHECK ((type = 'CommentReported' AND comment_reported_id != NULL AND user_reported_id = NULL) OR
+											(type = 'UserReported' AND user_reported_id != NULL AND comment_reported_id = NULL))
 );
 
 CREATE TABLE Notification (
@@ -130,32 +130,6 @@ CREATE TABLE Project_categories (
 	category_id INTEGER NOT NULL
 );
 
-CREATE TABLE Project_tasks (
-	project_id INTEGER NOT NULL,
-	task_id INTEGER NOT NULL
-);
-
-CREATE TABLE Sprint_tasks (
-	sprint_id INTEGER NOT NULL,
-	task_id INTEGER NOT NULL
-);
-
-CREATE TABLE Task_comments (
-	task_id INTEGER NOT NULL,
-	comment_id INTEGER NOT NULL
-);
-
-CREATE TABLE Thread_comments (
-	thread_id INTEGER NOT NULL,
-	comment_id INTEGER NOT NULL
-);
-
-CREATE TABLE Contains_image (
-	task_id INTEGER NOT NULL,
-	image_id INTEGER NOT NULL 
-);
-
-
 
 /* Primary Keys and Uniques*/
 
@@ -192,9 +166,6 @@ ALTER TABLE ONLY Project_members
 ALTER TABLE ONLY Administrator
 	ADD CONSTRAINT administrator_pkey PRIMARY KEY (id);
 
-ALTER TABLE ONLY Image
-	ADD CONSTRAINT image_pkey PRIMARY KEY (id);
-
 ALTER TABLE ONLY Report
 	ADD CONSTRAINT report_pkey PRIMARY KEY (id);
 
@@ -212,22 +183,7 @@ ALTER TABLE ONLY SprintStateRecord
 
 ALTER TABLE ONLY Project_categories
 	ADD CONSTRAINT project_categories_pkey PRIMARY KEY (project_id, category_id);
-
-ALTER TABLE ONLY Project_tasks
-	ADD CONSTRAINT project_tasks_pkey PRIMARY KEY (project_id, task_id);
-
-ALTER TABLE ONLY Sprint_tasks
-	ADD CONSTRAINT sprint_tasks_pkey PRIMARY KEY (sprint_id, task_id);
-
-ALTER TABLE ONLY Task_comments
-	ADD CONSTRAINT task_comments_pkey PRIMARY KEY (task_id, comment_id);
-
-ALTER TABLE ONLY Thread_comments
-	ADD CONSTRAINT thread_comments_pkey PRIMARY KEY (thread_id, comment_id);
-
-ALTER TABLE ONLY Contains_image
-	ADD CONSTRAINT contains_image_pkey PRIMARY KEY (task_id, image_id);
-
+ 	
 
 /* Foreign Keys */
 
@@ -236,6 +192,12 @@ ALTER TABLE ONLY Sprint
 
 ALTER TABLE ONLY Sprint
 	ADD CONSTRAINT sprint_id_user_creator_fkey FOREIGN KEY (user_creator_id) REFERENCES User(id) ON UPDATE CASCADE; 
+
+ALTER TABLE ONLY Task
+	ADD CONSTRAINT task_id_user_project_fkey FOREIGN KEY (project_id) REFERENCES Project(id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE ONLY Task
+	ADD CONSTRAINT task_id_user_sprint_fkey FOREIGN KEY (sprint_id) REFERENCES Sprint(id) ON UPDATE CASCADE;
 
 ALTER TABLE ONLY Thread
 	ADD CONSTRAINT thread_id_project_fkey FOREIGN KEY (project_id) REFERENCES Project(id) ON UPDATE CASCADE ON DELETE CASCADE;
@@ -246,6 +208,12 @@ ALTER TABLE ONLY Thread
 ALTER TABLE ONLY Comment
 	ADD CONSTRAINT comment_id_user_fkey FOREIGN KEY (user_id) REFERENCES User(id) ON UPDATE CASCADE;
 
+ALTER TABLE ONLY Comment
+	ADD CONSTRAINT comment_id_task_fkey FOREIGN KEY (task_id) REFERENCES Task(id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE ONLY Comment
+	ADD CONSTRAINT comment_id_thread_fkey FOREIGN KEY (thread_id) REFERENCES Thread(id) ON UPDATE CASCADE ON DELETE CASCADE;
+
 ALTER TABLE ONLY Project_members
 	ADD CONSTRAINT members_id_user_fkey FOREIGN KEY (user_id) REFERENCES User(id) ON UPDATE CASCADE ON DELETE CASCADE;
 
@@ -254,6 +222,12 @@ ALTER TABLE ONLY Project_members
 
 ALTER TABLE ONLY Report
 	ADD CONSTRAINT report_id_user_fkey FOREIGN KEY (user_id) REFERENCES User(id) ON UPDATE CASCADE;
+
+ALTER TABLE ONLY Report
+	ADD CONSTRAINT report_id_comment_reported_fkey FOREIGN KEY (comment_reported_id) REFERENCES Comment(id) ON UPDATE CASCADE;
+
+ALTER TABLE ONLY Report
+	ADD CONSTRAINT report_id_user_reported_fkey FOREIGN KEY (user_reported_id) REFERENCES User(id) ON UPDATE CASCADE;
 
 ALTER TABLE ONLY Notification
 	ADD CONSTRAINT notification_id_user_fkey FOREIGN KEY (user_id) REFERENCES User(id) ON UPDATE CASCADE ON DELETE CASCADE;    
@@ -290,27 +264,3 @@ ALTER TABLE ONLY Project_categories
 
 ALTER TABLE ONLY Project_categories
 	ADD CONSTRAINT project_categories_id_category_fkey FOREIGN KEY (category_id) REFERENCES Category(id) ON UPDATE CASCADE ON DELETE CASCADE;
-
-ALTER TABLE ONLY Project_tasks
-	ADD CONSTRAINT project_tasks_id_project_fkey FOREIGN KEY (project_id) REFERENCES Project(id) ON UPDATE CASCADE ON DELETE CASCADE;
-
-ALTER TABLE ONLY Project_tasks
-	ADD CONSTRAINT project_tasks_id_task_fkey FOREIGN KEY (task_id) REFERENCES Task(id) ON UPDATE CASCADE ON DELETE CASCADE;
-
-ALTER TABLE ONLY Sprint_tasks
-	ADD CONSTRAINT sprint_tasks_id_sprint_fkey FOREIGN KEY (sprint_id) REFERENCES Sprint(id) ON UPDATE CASCADE ON DELETE CASCADE;
-
-ALTER TABLE ONLY Sprint_tasks
-	ADD CONSTRAINT sprint_tasks_id_task_fkey FOREIGN KEY (task_id) REFERENCES Task(id) ON UPDATE CASCADE ON DELETE CASCADE;
-
-ALTER TABLE ONLY Task_comments
-	ADD CONSTRAINT task_comments_id_task_fkey FOREIGN KEY (task_id) REFERENCES Task(id) ON UPDATE CASCADE ON DELETE CASCADE;
-
-ALTER TABLE ONLY Task_comments
-	ADD CONSTRAINT task_comments_id_comment_fkey FOREIGN KEY (comment_id) REFERENCES Comment(id) ON UPDATE CASCADE ON DELETE CASCADE;	
-
-ALTER TABLE ONLY Contains_image
-	ADD CONSTRAINT contains_image_id_task_fkey FOREIGN KEY (task_id) REFERENCES Task(id) ON UPDATE CASCADE ON DELETE CASCADE;
-
-ALTER TABLE ONLY Task_comments
-	ADD CONSTRAINT task_comments_id_image_fkey FOREIGN KEY (image_id) REFERENCES Image(id) ON UPDATE CASCADE ON DELETE CASCADE;
