76b336fca7dab79796d4cee3c7ce9ea6a3418bb3
beemargarida <up201505505@gcloud.fe.up.pt>
2018-04-11 10:58:25 +0100

    started profile page

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -11,26 +11,75 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
-    <link href="{{ asset('css/milligram.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/common.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/profile_page.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
     <script type="text/javascript">
         // Fix for Firefox autofocus CSS bug
         // See: http://stackoverflow.com/questions/18943276/html-5-autofocus-messes-up-css-loading/18945951#18945951
     </script>
-    <script type="text/javascript" src={{ asset('js/app.js') }} defer>
-</script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+    <!--<script type="text/javascript" src={{ asset('js/app.js') }} defer></script> -->
   </head>
   <body>
-    <main>
-      <header>
-        <h1><a href="{{ url('/cards') }}">Thingy!</a></h1>
+    <nav class="row">
+    <a class="col-3" href="{{ url('/')}}">Proyekt</a>
+
+    <!--  FAZER ESTE FORM ATIVO -->
+    <form class="col-6">
+      <input class="form-control" type="text" placeholder="Search">
+      <button class="btn btn-primary" type="submit">
+        <i class="fas fa-search"></i>
+      </button>
+    </form>
+
+    <div id="user" class="col-3">
+      <input type="checkbox" id="hamburger">
+      <label class="hamburger" for="hamburger"></label>
+      <div id="mobile">
+        <button id="notification">
+          <i class="fas fa-bell"></i>
+        </button>
+        <div class="dropdown">
+          <div class="dropdown-content">
+            <div class="modal">
+              <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title">Notifications</h5>
+
+                    <!-- FAZER ISTO -->
+                    <!--<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                    </div>
+                    <div class="modal-body">
+                      <p><i class="far fa-paper-plane"></i>&nbsp; &nbsp;jotacl has invited you to project Management App</p>
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-success">Accept</button>
+                      <button type="button" class="btn btn-danger" data-dismiss="modal">Decline</button>
+                    </div>
+                    <div class="modal-body">
+                      <p><i class="far fa-paper-plane"></i>&nbsp; &nbsp;jotacl has promoted you to Project Coordinator of the project Management App</p>
+                    </div>-->
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>  
         @if (Auth::check())
-        <a class="button" href="{{ url('/logout') }}"> Logout </a> <span>{{ Auth::user()->username }}</span>
+          @if (Auth::user()->image == NULL)
+            <img src="res/profile/profile-pic.png">
+          @else
+            <img src="{{url(Auth::user()->image)}}">
+          @endif
         @endif
-      </header>
-      <section id="content">
-        @yield('content')
-      </section>
-    </main>
+      </div>
+    </div>
+  </nav>
+
+  @yield('content');
+  
   </body>
 </html>
