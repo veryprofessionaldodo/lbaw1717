a34e4df3662240b32dabf23efe89236ab07aaf4b
beemargarida <up201505505@gcloud.fe.up.pt>
2018-04-12 11:12:51 +0100

    vendor

--- src/app/User.php
+++ src/app/User.php
@@ -42,6 +42,20 @@ class User extends Authenticatable
     }
 
     public function getUserProjects(string $username, int $n){
+      return DB:raw('SELECT project.name, project.description, project_members.isCoordinator
+                      , num.num_members, sprints.sprints_num
+                      FROM "user", project_members, project
+                        INNER JOIN 
+                        (SELECT project_id, COUNT(project_id) AS num_members
+                        FROM project_members GROUP BY project_members.project_id) num 
+                        ON project.id = num.project_id
+                        INNER JOIN
+                        (SELECT project_id, COUNT(*) AS sprints_num FROM sprint
+                        GROUP BY project_id) sprints 
+                        ON project.id = sprints.project_id
+                      WHERE "user".username = $username AND project_members.user_id = "user".id 
+                      AND project_members.project_id = project.id AND num.project_id = project.id
+                      LIMIT 5 OFFSET $n;')
         /*return DB::select('project.name', 'project.description', 'project_members.isCoordinator')
                       ->from('project')
                       ->from('project_members')
