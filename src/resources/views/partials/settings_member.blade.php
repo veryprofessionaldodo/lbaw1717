c65fd21875b1b23a4ba5fc9b7e4d640510bfeb89
Pedro Daniel Reis <up201506046@fe.up.pt>
2018-05-14 13:13:01 +0100

    Bug fixed on admin and members, better mobile

--- src/resources/views/partials/settings_member.blade.php
+++ src/resources/views/partials/settings_member.blade.php
@@ -1,4 +1,4 @@
-<div data-id="{{$member->username}}" class="member row">
+<div data-id="{{$member->username}}" class="member-row">
     <div class="col-lg-6 col-12">
         <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png">
         <h3>{{$member->username}}</h3>
