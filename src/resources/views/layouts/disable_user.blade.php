819e8c87f603ef79b749e247f315b8e48020f36a
Vicente Espinha <up201503764@fe.up.pt>
2018-05-28 00:19:19 +0100

    just failing in life:sob: :disappointed:
    tentativa de mostar uma mensagem a dizer que o utilizador tinha sido banido. Só estou a dar commit a isto porque tenho medo de o perder

---
+++ src/resources/views/layouts/disable_user.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>Proyekt Index</title>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js" defer></script>
+            <script type="text/javascript" src={{ asset('js/disable_user_message.js') }} defer></script>
+    </head>
+    <body>
+    </body>
+</html>
